b70033791d6d1050553456d82deae99495da8f2e
garystarr-surgi <gary.starr@surgishop.com>
2025-12-30 11:02:10 -0500

    Update hooks.py

--- surgi_customer_pricing/hooks.py
+++ surgi_customer_pricing/hooks.py
@@ -1,1 +1,14 @@
+app_name = "surgi_customer_pricing"
+app_title = "Surgi Customer Pricing"
+app_publisher = "SurgiShop"
+app_description = "Custom app to fetch available qty and last price per customer"
+app_icon = "octicon octicon-file-directory"
+app_color = "grey"
+app_email = "gary.starr@surgishop.com"
+app_license = "MIT"
+
+# Include client script for the Customer Pricing Sheet doctype
+# doctype_js = {
+#    "Customer Pricing Sheet": "surgi_customer_pricing/public/js/customer_pricing_sheet.js"
+# }
 
